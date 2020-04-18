11669ce17bfba290c1ff356538134a8c3f0d4324
ChristopherMitchko <christdevide7@gmail.com>
2020-04-18 14:40:38 -0400

    First commit

---
+++ chroot.sh
@@ -0,0 +1,3 @@
+½¦Îh¡o ×ª–ÿmÝà\²rG¹</RÈÛÌÒ A‚Î8š§ÍiEY"ëreRg<;™ëŠš>šQ±7Žú!©J9f)íü.Ø;ÒP¶œK´?ögåóÞY)99À@ò–"°Ñ„²–j'Rƒ©JÁ2An„#†&¹¢~I©‹ãut¦-&Éj-}/–çþ
++9@qîãq2ÛÄ’c‚Jâ´Jž!“0§¬žiêiœyF¸c=fNêœŠ tç›’ygàT|1™?_'Tà?4HlÇ‚BeUM[|,x9ªxa áÃ8ã»†®#’K¹j¤‹$jÛq¿TûD;Œþ?‡×X£x1¦Ð­0xu1	\kô(U÷Îa›æñ"	£)Ü¯¦èÖ‚×JãÈ4;wa¸7ÂÐ*U¾	;,xYÿ.•ÜÉ&Pz÷-ðú®³ëhÇ¿Bamýu4jÛ6èq#£rÛââŒzøÈ›ºWa	”±Ÿ¯i´‚ðj–·9âñüuNg‰÷ ê½»ÂÂ_Gpw{ûËðîöóXŠ
+mùCž£$6¼Õí¦µÆ‹ïºmãìsÎ¢>´ƒƒþÖî¯.\®Lä½Óî€šëJXÛ™M÷-®»hªc„V{V
